d32aa170ec31868156c749e62ff553951b2f9f53
bardarz <aspha9@outlook.com>
2018-11-04 22:19:20 +0100

    add status wallet info

--- src/controllers/status.controller.js
+++ src/controllers/status.controller.js
@@ -10,7 +10,7 @@ import {
 
 
 // Get returns a rippled info status
-const Get = async (req, res) => {
+const GetInfo = async (req, res) => {
     console.log("info route")
     try {
         Ripple.connect().then(() => {
@@ -29,7 +29,28 @@ const Get = async (req, res) => {
     }
 };
 
+const GetWalletInfo = async (req, res) => {
+    const walletID = req.params.ID;
+    console.log(walletID)
+    try {
+        Ripple.connect().then(() => {
+
+            // Request account_info, log response...
+            Ripple.getAccountInfo(walletID).then(info => {
+                console.log(info);
+                res.status(200).json(info);
+            });
+        });
+        
+    } catch(e) {
+        res.status(500).json({
+            error: ErrInternalServerError
+        });
+    }
+}
+
 
 module.exports = {
-    Get
+    GetInfo,
+    GetWalletInfo
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,8 @@ import StatusController from '../controllers/status.controller';
 module.exports = (app) => {
 
     // Status
-    app.get('/api/status/info', StatusController.Get);
+    app.get('/api/status/info', StatusController.GetInfo);
+    app.get('/api/status/wallet/:ID', StatusController.GetWalletInfo);
 
 
 };
